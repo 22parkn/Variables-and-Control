c109982f7ed5ac62c36724892d202ad69dd9e1a2
22parkn <22parkn@flintridgeprep.org>
2020-09-29 17:45:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+class Main {
+  public static void main(String[] args) {
+    /*
+  int x+=2;
+  boolean y=true;
+  double z*=10.1;
+  System.out.println(x);
+  System.out.println(y);
+  System.out.println(z);
+  char w='p';
+  System.out.println(w);
+  /*this is a comment
+*/
+
+  int x = 1;
+   System.out.println(x);
+  while (x<500) {
+    x += 1;
+  int y = x * x; 
+    System.out.println(y);
+    if (y == 25); {
+      System.out.println("WOW!");
+    }
+  }
+
+  int n = 20;
+	for (int i = 1; i <= n; i++) {
+	   if (i % 2 != 0) {
+		System.out.print(i + ",");
+    int[] testArray = {1, 2, 3, 5, 7, 11, 13, 17, 19};
+    if (boolean (n = 7=true););
+    System.out.print ("I get it, they're all squares!");
+    }
+    }
+  }
+}
